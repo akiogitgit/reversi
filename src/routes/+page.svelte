56c792bdf73991fb4d278a0bfd88edc3154e16b0
akiogitgit <akimaron2828@icloud.com>
2023-05-19 22:31:47 +0900

    feat: 順番に石を置けるように

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,17 @@
 	// 盤上全て埋まるか、どちらも挟めないなら終了
 	// 石の多い方の勝利。引き分けもある。
 
-	import BlackStone from '../components/BlackStone.svelte';
-	import WhiteStone from '../components/WhiteStone.svelte';
+	import BlackStone from '../components/BlackStone.svelte'
+	import WhiteStone from '../components/WhiteStone.svelte'
 
 	// 考えること
 	// 1. 盤上を表示
+	// 2. 順番に押す
+	// 3. 押せる場所を挟める場所のみに
+	// 4. 挟んだらひっくり返す
+	// 5. 勝敗
 
-	type Field = null | '黒' | '白';
+	type Field = null | '黒' | '白'
 	let fields: Field[][] = [
 		[null, null, null, null, null, null, null, null],
 		[null, null, null, null, null, null, null, null],
@@ -23,29 +27,44 @@
 		[null, null, null, null, null, null, null, null],
 		[null, null, null, null, null, null, null, null],
 		[null, null, null, null, null, null, null, null]
-	];
+	]
+
+	let nextPlayer: '黒' | '白' = '黒'
+
+	// プレイヤーを交互に
+	const changePlayer = () => {
+		nextPlayer = nextPlayer === '白' ? '黒' : '白'
+	}
+
+	// フィールドを押す
+	const onClickField = (y: number, x: number) => {
+		if (fields[y][x]) return
+		fields[y][x] = nextPlayer
+		changePlayer()
+	}
 </script>
 
 <section>
 	<h1 class="font-bold text-center text-40px">リバーシ</h1>
 
 	<div class="border-black rounded-md bg-green-500 border-8 mt-8">
-		{#each fields as field1, i (i)}
+		{#each fields as field1, y (y)}
 			<div class="flex">
-				{#each field1 as field, i (i)}
-					<div
+				{#each field1 as field, x (x)}
+					<button
 						class="border border-black h-11vw max-h-60.5px grid w-11vw place-items-center sm:(h-60.5px w-60.5px)"
+						on:click={() => onClickField(y, x)}
 					>
 						{#if field === '白'}
 							<WhiteStone />
 						{:else if field === '黒'}
 							<BlackStone />
 						{/if}
-					</div>
+					</button>
 				{/each}
 			</div>
 		{/each}
 	</div>
 
-	<p class="mt-8 text-center text-xl">黒の番です</p>
+	<p class="mt-8 text-center text-xl">{nextPlayer === '白' ? '黒' : '白'}の番です</p>
 </section>

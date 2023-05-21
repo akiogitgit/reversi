908ffa63ac616b33aaccd094bf1715f9938e44ef
akiogitgit <akimaron2828@icloud.com>
2023-05-21 11:09:28 +0900

    feat: meta情報、faviconを設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,13 @@
 </script>
 
 <svelte:head>
-	<title>オセロ</title>
-	<meta name="description" content="オセロ" />
+	<title>リバーシ</title>
+	<meta name="description" content="リバーシ" />
+	<link rel="icon" href="/reversi.webp" />
+	<meta name="keywords" content="オセロ, リバーシ, オセロゲーム" />
+	<meta property="og:image" content="/reversi.webp" />
+	<meta property="og:site_name" content="リバーシ" />
+	<meta property="og:locale" content="ja_JP" />
 </svelte:head>
 
 <main class="mx-auto mt-8 max-w-500px w-90vw">
